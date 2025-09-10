2220e1268298a4e0972bcb8d6226fa749047dde7
Jayasni530 <147836343+Jayasni530@users.noreply.github.com>
2025-09-10 11:09:13 +0530

    Add files via upload

---
+++ src/main/java/com/CloudFood/cloudfood/CloudfoodApplication.java
@@ -0,0 +1,13 @@
+package com.CloudFood.cloudfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CloudFood/cloudfood/CloudfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CloudFood.cloudfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
